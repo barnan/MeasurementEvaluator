274510771acc577c61ae81416481e34c15b9e200
barno <barno@DESKTOP-37EEL3L>
2017-01-31 09:03:39 +0100

    TTR read meas data tester in progress

--- ClassLibrary1/DAL_Tester/TTRDataReaderTester.cs
+++ ClassLibrary1/DAL_Tester/TTRDataReaderTester.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,10 +11,37 @@ namespace ClassLibrary1.DAL_Tester
     [TestFixture]
     class TTRDataReaderTester
     {
-        [SetUpFixture]
+        string folder1;
+
+        [OneTimeSetUp]
+        public void CopyFiles()
+        {
+            string path = Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().GetName().CodeBase);
+            DirectoryInfo dirName = Directory.GetParent(Directory.GetParent(path.Substring(6, path.Length - 6)).ToString());
+
+            folder1 = dirName.ToString() + "\\DAL_Tester\\TTR_InputData";
 
 
 
+        }
+
+        
+        [OneTimeTearDown]
+        public void DeleteFiles()
+        {
+
+
+        }
+
+
+
+        [Test, Category("MeasurementDataReading")]
+        public void TestNumberOfColumnsAndRows()
+        {
+            
+
+
+        }
 
     }
 }
