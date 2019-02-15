65200672f9964b63aa38d9a621e3b5e8ee690c5f
Norbert Barna <norbert.barna@semilab.hu>
2019-02-15 15:41:06 +0100

    added UI and startup project

---
+++ MeasurementEvaluator/Program.cs
@@ -0,0 +1,54 @@
+﻿using MeasurementEvaluatorUI;
+using System;
+using System.Diagnostics;
+using System.Threading;
+using System.Windows;
+
+namespace MeasurementEvaluator
+{
+    class Program
+    {
+        private static ManualResetEvent _uiFinished = new ManualResetEvent(false);
+
+        static void Main(string[] args)
+        {
+            try
+            {
+                Thread appThread = new Thread(() =>
+                {
+                    Application application = new Application();
+
+                    MainWindow mainWindow = new MainWindow { Title = "Measurement Evaluator UI" };
+                    mainWindow.Closed += MainWindow_OnClosed;
+
+                    System.Windows.Application.Current.MainWindow = mainWindow;
+                    mainWindow.Show();
+                    application.Run(mainWindow);
+
+                });
+                Debug.Assert(appThread != null);
+                appThread.Name = "WpfThread";
+                appThread.SetApartmentState(ApartmentState.STA);
+                appThread.IsBackground = true;
+                appThread.Start();
+
+                _uiFinished.WaitOne();
+
+            }
+            catch (Exception ex)
+            {
+
+            }
+            finally
+            {
+
+            }
+
+        }
+
+        private static void MainWindow_OnClosed(object sender, EventArgs eventArgs)
+        {
+            _uiFinished.Set();
+        }
+    }
+}
---
+++ MeasurementEvaluatorUI/App.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System.Windows;
+using System.Windows.Threading;
+
+namespace MeasurementEvaluatorUI
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        private void App_OnStartup(object sender, StartupEventArgs e)
+        {
+            MainWindow mw = new MainWindow();
+            mw.Title = "Measurement Evaluator UI";
+            mw.Show();
+        }
+
+        private void App_OnDispatcherUnhandledException(object sender, DispatcherUnhandledExceptionEventArgs e)
+        {
+            MessageBox.Show("An unhandled exception just occurred: " + e.Exception.Message, "Exception Sample", MessageBoxButton.OK, MessageBoxImage.Warning);
+            e.Handled = true;
+        }
+    }
+}
